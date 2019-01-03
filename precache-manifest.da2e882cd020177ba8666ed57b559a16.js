ccadfc9e940736eb8175e7d35a4f08afec12c798
rajchunara <rajchunara777@gmail.com>
2019-01-03 13:28:33 -0800

    Updates

---
+++ precache-manifest.da2e882cd020177ba8666ed57b559a16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8b9e1eb47acdb30607c",
+    "url": "/Population-Stats/static/js/runtime~main.b8b9e1eb.js"
+  },
+  {
+    "revision": "128179de5b7e38e4137c",
+    "url": "/Population-Stats/static/js/main.128179de.chunk.js"
+  },
+  {
+    "revision": "e3299d85aa08c977efd6",
+    "url": "/Population-Stats/static/js/1.e3299d85.chunk.js"
+  },
+  {
+    "revision": "128179de5b7e38e4137c",
+    "url": "/Population-Stats/static/css/main.edd76fce.chunk.css"
+  },
+  {
+    "revision": "3ad78ae33e5009f49c3b96621d855535",
+    "url": "/Population-Stats/index.html"
+  }
+];
